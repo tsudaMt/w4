4323d54f2aa2c85b8ba257296eea8f2cdd5b9c81
Coromoo <113568980+tsudaMt@users.noreply.github.com>
2022-10-11 12:17:17 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'その日は華氏94度と暑かった, なので :insertx: 散歩に出かけた. 彼らがついた時、 :inserty:, 彼らはしばらくの間恐怖に立ち尽くしていたが、 :insertz:. ボブは一部始終を見ていたが、特段驚かなかった — :insertx: weighs 300 pounds, and it was a hot day.';
+const storyText = 'その日は華氏94度と暑かった, なので :insertx: 散歩に出かけた. 彼らがついた時、 :inserty:, 彼らはしばらくの間恐怖に立ち尽くしていたが、 :insertz:. ボブは一部始終を見ていたが、特段驚かなかった。 — :insertx: 体重は300ポンドあって、その日はとても暑かったからだ。';
 const insertX = ['グリーン・ゴブリン', '太った男', 'サンタクロース'];
 const insertY = ['教会の炊き出し', 'ディズニーランド', 'ホワイトハウス'];
 const insertZ = ['自然発火した', '歩道の水たまりに溶けてしまった', 'なめくじになってどこかに行ってしまった'];
