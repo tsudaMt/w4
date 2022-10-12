5f6927abe046d8e563dc0f6245f3eb5000579ab8
Coromoo <113568980+tsudaMt@users.noreply.github.com>
2022-10-12 13:19:00 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -33,7 +33,7 @@ function result() {
 
   if (document.getElementById("uk").checked) {
     const weight = `${Math.round(300*0.0714286)} stone`;
-    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+    const temperature =  `摂氏${Math.round((94-32) * 5 / 9)} 度`;
     newStory = newStory.replace('華氏94度', temperature);
     newStory = newStory.replace('300ポンド', weight);
   }
